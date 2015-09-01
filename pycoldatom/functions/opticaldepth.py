24af6904086eda06092b8044e0f1cbe2f0ec89ff
duguxy <duguxy@gmail.com>
2015-09-01 18:08:57 +0800

    fix od

--- pycoldatom/functions/opticaldepth.py
+++ pycoldatom/functions/opticaldepth.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def calculateOD(sig, ref, bkg, min_step=1):
+def calculateOD(sig, ref, bkg):
+	min_step = 1
 	sig = np.maximum(sig-bkg, min_step)
 	ref = np.maximum(ref-bkg, min_step)
 	od = -np.log(sig/ref)
