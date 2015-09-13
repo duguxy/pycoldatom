Fix: Int overflow in fringeremove

pycoldatom/functions/fringe.py
@@ -19,18 +19,29 @@ class FringeRemove:
 		U, s, V = self.svd_result
 		self.rank = len(s)
 
+	# def reconstruct(self, image):
+	# 	U, s, V = self.svd_result
+	# 	y = np.ma.compressed(image)[:, np.newaxis]
+	# 	if hasattr(image, 'mask'):
+	# 		mask = image.mask.flatten()
+	# 		q, r = npl.qr(U[np.logical_not(mask)])
+	# 		inv = npl.pinv(r * s)
+	# 		x = np.dot(V, inv).dot(q.T).dot(y)
+	# 	# y = np.ma.filled(image, fill_value=0).flatten()[:, np.newaxis]
+	# 	else:
+	# 		x = np.dot(V * 1/s, U.T).dot(y)
+	# 	return x.flatten(), np.dot(U * s, V.T).dot(x).flatten()
+
 	def reconstruct(self, image):
 		U, s, V = self.svd_result
 		y = np.ma.compressed(image)[:, np.newaxis]
 		if hasattr(image, 'mask'):
 			mask = image.mask.flatten()
-			q, r = npl.qr(U[np.logical_not(mask)])
-			inv = npl.pinv(np.dot(r, np.diag(s)))
-			x = np.dot(V, inv).dot(q.T).dot(y)
-		# y = np.ma.filled(image, fill_value=0).flatten()[:, np.newaxis]
+			U1 = U[np.logical_not(mask)]
 		else:
-			x = np.dot(V, np.diag(1/s)).dot(U.T).dot(y)
-		return x.flatten(), np.dot(U, np.diag(s)).dot(V.T).dot(x).flatten()
+			U1 = U
+		x = np.dot(npl.pinv(U1), y)
+		return x.flatten(), np.dot(U, x)
 
 	def setTrunc(self, trunc):
 		self.trunc = trunc

pycoldatom/functions/incremental_svd.py
@@ -2,7 +2,7 @@ import numpy as np
 import numpy.linalg as npl
 from scipy.linalg import block_diag
 
-trunc = 1.0
+trunc = 0.0
 
 def incre_svd():
 	""" Incremental SVD generator, see 
@@ -11,7 +11,8 @@ def incre_svd():
 	"""
 
 	c = yield
-	s = np.array([npl.norm(c)])
+	s = np.array([npl.norm(c.astype(float))])
+	# s = npl.norm(c.astype(float), axis=1)
 	U0 = c / s
 	Up = 1.0
 	V0 = 1.0
@@ -23,7 +24,6 @@ def incre_svd():
 		U = np.dot(U0, Up)
 		V = np.dot(V0, Vp)
 		c = yield U, s, V
-
 		if c is None:
 			continue
 